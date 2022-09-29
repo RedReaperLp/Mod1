bc58285134a59289ee614739b764b3a6d11c28f0
Raphael Kuschmierz <fiffykuschmierz@gmail.com>
2022-09-29 19:54:55 +0200

    -

--- docs/css/script.js
+++ docs/css/script.js
@@ -1,10 +1,20 @@
-
-
-function onButtonClick() {
- window.location.assign("https://github.com/RedReaperLp/Mod1/");
+function changeFavicon(src) {
+  var link = document.createElement('link'),
+    oldLink = document.getElementById('icon');
+  link.id = 'dynamic-favicon';
+  link.rel = 'shortcut icon';
+  link.href = src;
+  if (oldLink) {
+    document.head.removeChild(oldLink);
+  }
+  document.head.appendChild(link);
 }
 
-window.onload = function() {
+function onButtonClick() {
+  window.location.assign("https://github.com/RedReaperLp/Mod1/");
+}
+
+window.onload = function () {
 
 }
 
